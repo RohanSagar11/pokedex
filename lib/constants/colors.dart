9b17e2c32f7bb4f557ca92e47767473be955f2fe
rohansagar11 <captainnerd789@gmail.com>
2024-12-27 23:58:58 +0530

    2nd commit, updated mainscreen ui

---
+++ lib/constants/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color primary = Colors.red;
+const Color bug = Colors.lightGreen;
+const Color dark = Colors.brown;
+const Color dragon = Colors.blueAccent;
+const Color electric = Colors.yellowAccent;
+ Color? box = Colors.grey[300];
